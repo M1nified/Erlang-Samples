d15d8567223872104ebeced7df647d639f40db68
Michal Gora <goramich@borg.kis.agh.edu.pl>
2016-10-18 18:28:27 +0200

    benchmark

--- zad1.erl
+++ zad1.erl
@@ -1,6 +1,30 @@
 -module(zad1).
 -export([
-    randlist/1
+    randlist/1,
+    qsort/1,
+    benchmarksort/0
 ]).
 
-randlist(Num) -> [random:uniform(100) || _ <- lists:seq(1, Num)].
+% list lenght
+len([]) -> 0;
+len(List) -> len(List,0).
+
+len([],Len) -> Len;
+len([_|Tail],Len) -> len(Tail,Len+1). 
+
+randlist(Num) -> [rand:uniform(100) || _ <- lists:seq(1, Num)].
+
+qsort([]) -> [];
+qsort([Pivot|T]) ->
+    qsort([X || X <- T, X < Pivot])
+    ++ [Pivot] ++
+    qsort([X || X <- T, X >= Pivot]).
+
+% sort(Parent,List) ->
+%     if len(List)>2  
+%         % split
+%     else
+%         % sort & send
+
+benchmarksort()->
+    benchmark:test_avg(zad1,qsort,[randlist(100)],10).
