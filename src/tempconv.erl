63e2d30cb99555d056d4798e153e6175f4e147b3
Minified <gora.pub@gmail.com>
2016-10-28 13:13:39 +0200

    convert_seq more depending

--- src/tempconv.erl
+++ src/tempconv.erl
@@ -12,13 +12,13 @@
 % r - Rankine
 convert_seq(Val,c,f) -> Val * 9 / 5 + 32;
 convert_seq(Val,c,k) -> Val + 273.15;
-convert_seq(Val,c,r) -> convert_seq(Val,c,k) * 9 / 5;
+convert_seq(Val,c,r) -> convert_seq(convert_seq(Val,c,k),k,r); %convert_seq(Val,c,k) * 9 / 5;
 
 convert_seq(Val,f,c) -> (Val - 32) * 5 / 9;
-convert_seq(Val,f,k) -> convert_seq(Val,f,r) * 5 / 9;
+convert_seq(Val,f,k) -> convert_seq(convert_seq(Val,f,r),r,k); %convert_seq(Val,f,r) * 5 / 9;
 convert_seq(Val,f,r) -> Val + 459.67;
 
-convert_seq(Val,k,f) -> convert_seq(Val,k,r) - 459.67;
+convert_seq(Val,k,f) -> convert_seq(convert_seq(Val,k,r),r,f); %convert_seq(Val,k,r) - 459.67;
 convert_seq(Val,k,c) -> Val - 273.15;
 convert_seq(Val,k,r) -> Val * 9 / 5;
 
