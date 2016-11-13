87af1633da1838cfd3944f4dc964fda1d12f7f96
Minified <gora.pub@gmail.com>
2016-11-13 22:19:54 +0100

    line break fix

--- test.sh
+++ test.sh
@@ -2,11 +2,7 @@
 SUITES=`ls ebin | grep _SUITE`
 SUITES=${SUITES//.beam/}
 SUITES="${SUITES[*]}"
-# echo $SUITES
 SUITES_STR=""
-for SUITE in $SUITES; do
-  SUITES_STR="$SUITES_STR, $SUITE"
-done
-# echo ${SUITES_STR#, }
+for SUITE in $SUITES; do SUITES_STR="$SUITES_STR, $SUITE"; done
 erl -noshell -pa ebin -eval "case eunit:test([${SUITES_STR#, }], [verbose]) of ok -> init:stop(0); {error,_} -> init:stop(1) end."
 # erl -noshell -pa ebin -eval "eunit:test(pmap_SUITE, [verbose])" -s init stop
