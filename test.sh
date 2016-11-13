be15dbbf6711c156902abfddf3bace6b671e75c3
Minified <gora.pub@gmail.com>
2016-11-13 21:59:19 +0100

    travis fail test

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ for SUITE in $SUITES; do
   SUITES_STR="$SUITES_STR, $SUITE"
 done
 # echo ${SUITES_STR#, }
-erl -noshell -pa ebin -eval "eunit:test([${SUITES_STR#, }], [verbose])" -s init stop
+erl -noshell -pa ebin -eval "case eunit:test([${SUITES_STR#, }], [verbose]) of ok -> init:stop(0); {error,_} -> init:stop(1) end."
 # erl -noshell -pa ebin -eval "eunit:test(pmap_SUITE, [verbose])" -s init stop
