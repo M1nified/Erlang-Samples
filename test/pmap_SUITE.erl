8f89d4bd176c76d6447195e536fcb33f563ac522
Minified <gora.pub@gmail.com>
2016-11-13 21:08:42 +0100

    travis fail test

--- test/pmap_SUITE.erl
+++ test/pmap_SUITE.erl
@@ -2,6 +2,9 @@
 -include_lib("eunit/include/eunit.hrl").
 
 pmap__1_test() ->
-    [3,4,5,6] = pmap:pmap(fun (X) -> X + 2 end, [1,2,3,4]).
+    [3,4,5,6] = pmap:pmap(fun (X) -> X + 2 end, [1,2,3,4]),
+    ["A","B","C"] = pmap:pmap(fun (X) -> string:to_upper(X) end, ["a","b","C"]).
 pmap__2_test() ->
     {error, badarith} = pmap:pmap(fun (X) -> X + 2 end, [1,2,"w",4]).
+pmap__3_test() ->
+    {error, {badfun,3}} = pmap:pmap(2, [1,2,3,4]).
