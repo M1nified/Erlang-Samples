6c8c054380d5727e87927bc15c4177edd05264d1
Michał Góra <Micha Gra>
2016-11-29 16:26:24 +0100

    gen_srv state save/load

--- src/gen_srv.erl
+++ src/gen_srv.erl
@@ -3,6 +3,8 @@
     init/1
 ]).
 
+-define(STATE_FILE,"gen_srv_state").
+
 -record(loopobj,{
     state :: any(),
     jobmodule :: module()
@@ -11,11 +13,21 @@
 
 -spec init(atom()) -> pid().
 init(JobModule) ->
-    spawn(fun() -> loop(#loopobj{jobmodule=JobModule}) end).
+    RecentState = state_load(),
+    io:format("RecentState = ~p\n",[RecentState]),
+    case RecentState of
+        {error, _} ->
+            spawn(fun() -> loop(#loopobj{jobmodule=JobModule}) end);
+        {ok,_} ->
+            spawn(fun() -> loop(#loopobj{jobmodule=JobModule, state=RecentState}) end)
+    end.
+
 
 -spec loop(loopobj()) -> any().
 loop(Loop) ->
     receive
+        {debug,Any} ->
+            debug(Loop,Any);
         Any ->
             recv(Loop,Any)            
     end.
@@ -33,6 +45,10 @@ recv(Loop,{cast,From,Ref,Data}) when is_pid(From) and is_reference(Ref) ->
 recv(Loop,{update,From,Ref,Module}) when is_pid(From) and is_reference(Ref) and is_atom(Module) ->
     loop(Loop#loopobj{jobmodule=Module});
 
+recv(Loop,{save, From, Ref}) when is_pid(From) and is_reference(Ref) ->
+    state_save(Loop),
+    loop(Loop);
+
 recv(Loop,_) ->
     io:fwrite("Unknown command!\n"),
     loop(Loop).
@@ -42,3 +58,23 @@ just_do_it(Loop,Data) ->
     Result = JobModule:action(Data,Loop#loopobj.state),
     NewState = JobModule:state(Loop#loopobj.state,Result),
     {Result,NewState}.
+
+% Actions
+
+state_save(Loop) ->
+    file:write_file(?STATE_FILE,term_to_binary(Loop#loopobj.state)),
+    ok.
+
+state_load() ->
+    case file:read_file(?STATE_FILE) of
+        {ok, Binary} ->
+            {ok, binary_to_term(Binary)};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+% Debug
+
+debug(Loop,print_state) ->
+    io:format("Current state is: ~p\n",[Loop#loopobj.state]),
+    loop(Loop).
